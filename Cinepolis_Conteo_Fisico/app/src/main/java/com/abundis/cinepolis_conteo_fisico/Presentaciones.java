ab78fffe3eb4d5e7408b7b9ca3fc8ef67c8781b1
KeviinAbundiiz <44123659+KeviinAbundiiz@users.noreply.github.com>
2020-02-19 17:21:23 -0600

    Se agrega aplicación móvil

---
+++ Cinepolis_Conteo_Fisico/app/src/main/java/com/abundis/cinepolis_conteo_fisico/Presentaciones.java
@@ -0,0 +1,13 @@
+package com.abundis.cinepolis_conteo_fisico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Presentaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentaciones);
+    }
+}
---
+++ Cinepolis_Conteo_Fisico/app/src/main/java/com/abundis/cinepolis_conteo_fisico/almacen1.java
@@ -0,0 +1,13 @@
+package com.abundis.cinepolis_conteo_fisico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class almacen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_almacen1);
+    }
+}
